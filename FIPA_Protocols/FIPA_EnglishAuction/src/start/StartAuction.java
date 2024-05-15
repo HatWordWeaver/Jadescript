6f15a3e959f1279d818ec0e0df5b40a4c3248e1e
Riccardo Nazzari <riccardonazzari@gmail.com>
2024-05-15 16:02:12 +0200

    init

---
+++ FIPA_Protocols/FIPA_EnglishAuction/src/start/StartAuction.java
@@ -0,0 +1,45 @@
+package start;
+
+import java.util.Scanner;
+
+import agents.Auctioneer;
+import agents.Bidder;
+import jade.wrapper.ContainerController;
+import jadescript.java.Jadescript;
+
+public class StartAuction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try (Scanner scanner = new java.util.Scanner(System.in)){
+			
+			ContainerController container = Jadescript.newMainContainer(1099);
+			
+			// create Partecipant
+			Auctioneer.create(container, "Auctioneer", "Gigio", 10);
+			
+			
+			System.out.println("Auctioneer created! "
+					+ "Press any key to start the program");
+			
+			scanner.nextLine();
+			
+			// create Initiator 
+			Bidder.create(container, 
+					"Pietro", 60);
+			
+			Bidder.create(container, 
+					"Paolo", 50);
+			
+			Bidder.create(container, 
+					"Petrox", 70);
+			
+			Bidder.create(container, 
+					"Abbiate", 76);
+			
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}		
+	}
+}
